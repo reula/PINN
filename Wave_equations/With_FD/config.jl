bed2da3fa757a96b0bb7b2dd578953968b8ade7f
Oscar Reula <oreula@nabucodonosor.ccad.unc.edu.ar>
2025-10-27 19:26:06 -0300

    nothing relevant

--- Wave_equations/With_FD/config.jl
+++ Wave_equations/With_FD/config.jl
@@ -13,13 +13,14 @@ config_basic = Dict(
     :xmax => 1.0,           # = L dominio espacial
     :tmin => 0.0,           # t_min
     :tmax => 2.0,           # t_max
-    #:optimizer => BFGS(),
-    :optimizer => SSBroyden(),
+    :optimizer => BFGS(),
+    #:optimizer => SSBroyden(Optim.Options(linesearch=LineSearches.HagerZhang(), show_trace=true)),
+    #:optimizer => SSBroyden(),
     #:optimizer => AdaMax(; alpha=0.001, beta_mean=0.9, beta_var=0.999, epsilon=1e-8),
     #:optimizer => Adam(; alpha=0.002, beta_mean=0.9, beta_var=0.999, epsilon=1e-8),
     :maxiters => 3_000,
-    :N_rounds => 50,    # 5           # nº de rondas RAD
-    :iters_per_round => 500,       # iteraciones BFGS por ronda
+    :N_rounds => 40,    # 5           # nº de rondas RAD
+    :iters_per_round => 1000,       # iteraciones BFGS por ronda
     :k1 => 1.0, 
     :k2 => 1.0,        # hiperparámetros RAD
     :N_test => 30_000,             # candidatos por ronda, mayor que N_points
